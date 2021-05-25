f81b4d6408c98eee2480e7b931c6b99751294282
Sam Thompson <samthompson292@gmail.com>
2021-05-25 15:17:27 +0100

    fixed react format

--- budgetcog/frontend/src/index.js
+++ budgetcog/frontend/src/index.js
@@ -1,1 +1,19 @@
-import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom';
+
+
+ReactDOM.render(
+  <Router>
+    <React.StrictMode>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </React.StrictMode>
+  </Router>,
+  document.getElementById('root'));
