ae147c2a36950e3824e638f4bab0688250502697
Sam Thompson <samthompson292@gmail.com>
2021-05-25 16:59:17 +0100

    trying to fix graphql requests

--- budgetcog/frontend/src/components/GetUsers.js
+++ budgetcog/frontend/src/components/GetUsers.js
@@ -12,7 +12,7 @@ function GetUsers() {
 
     return (
         <div>
-
+            
         </div>
     )
 }
--- budgetcog/frontend/src/index.js
+++ budgetcog/frontend/src/index.js
@@ -6,14 +6,49 @@ import {
   BrowserRouter as Router,
   Switch
 } from 'react-router-dom';
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+  HttpLink, 
+  from 
+} from "@apollo/client";
+import { onError } from '@apollo/client/link/error';
 
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({
+    credentials: 'same-origin',
+    uri: "http://localhost:8000/graphql/",
+    fetchOptions: {
+      mode: 'no-cors',
+    },
+  }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+  connectToDevTools: true,
+});
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <Switch>
-        <Route exact path="/" component={App} />
-      </Switch>
-    </React.StrictMode>
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <React.StrictMode>
+        <Switch>
+          <Route exact path="/" component={App} />
+        </Switch>
+      </React.StrictMode>
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root'));
