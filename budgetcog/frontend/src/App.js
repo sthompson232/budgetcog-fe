7b85cf428a76931a04acd3cba1e58fbe8c145744
Sam Thompson <samthompson292@gmail.com>
2021-05-25 16:03:03 +0100

    added graphql to react

--- budgetcog/frontend/src/App.js
+++ budgetcog/frontend/src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+  HttpLink, 
+  from 
+} from "@apollo/client";
+import { onError } from '@apollo/client/link/error';
+import GetUsers from './components/GetUsers';
+
+const errorLink = onError(({ graphqlErrors, networkError}) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`GraphQL error ${message} at ${location}. Path is ${path}`)
+    })
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: "http://localhost:8000/graphql",
+    fetchOptions: {
+      mode: 'no-cors',
+    },
+  }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return (
+  <ApolloProvider client={client}>
     <div>
       <h1>Hello, World!</h1>
     </div>
+    <GetUsers />
+  </ApolloProvider>
   );
 }
 
---
+++ budgetcog/frontend/src/components/GetUsers.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { LOAD_USERS } from '../graphql/Queries';
+
+function GetUsers() {
+
+    const {error, loading, data} = useQuery(LOAD_USERS)
+
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default GetUsers
