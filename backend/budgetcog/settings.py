e651dd07c47cd906cfdc143318fa95bd8b2f22bb
Sam Thompson <samthompson292@gmail.com>
2021-07-16 19:13:07 +0100

    updated get request

--- backend/budgetcog/settings.py
+++ backend/budgetcog/settings.py
@@ -65,7 +65,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000"
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,33 @@ import { useEffect } from 'react';
 import { useSelector } from "react-redux";
 import Login from './components/auth/Login'
 import Logout from './components/auth/Logout'
+import axios from 'axios'
+
+
+const baseURL = 'http://127.0.0.1:8000/api/';
+
+const axiosInstance = axios.create({
+	baseURL: baseURL,
+	timeout: 5000,
+
+});
+
 
 function App() {
   let name = useSelector((state: any) => {return state.user.fullName})
   let image = useSelector((state: any) => {return state.user.imageUrl})
   let email = useSelector((state: any) => {return state.user.email})
 
-  const token = localStorage.getItem('access_token')
-
   useEffect(() => {
-    fetch('http://localhost:8000/api/test/', {
+    axios.get('http://localhost:8000/api/test/', {
       headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('access_token'),
         'Content-Type': 'application/json',
-        'Authorization': `Token ${token}`
+        accept: 'application/json',
       }
     })
-    .then(res => res.json())
     .then(res => console.log(res))
-  }, [token])
+  }, [])
 
   return (
     <div className="App">
