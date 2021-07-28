7992c681e79991ed372727137fdfebfc22393cca
Sam Thompson <samthompson292@gmail.com>
2021-07-28 15:54:30 +0100

    updated domain

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
 
 
-export const url = 'http://localhost:8000'
-// export const url = 'https://budgetcog.com'
+// export const url = 'http://localhost:8000'
+export const url = 'https://budgetcog.xyz'
